2e05eeedfcd7116afed3647a6cd33cc1041fd32c
assilbekov <askhat.assilbekov@gmail.com>
2024-04-17 13:05:24 -0300

    add config mustload

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"url-shortener-rest-practice/internal/config"
+)
+
 func main() {
 	// TODO: init config: cleanenv
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
 
 	// TODO: init logger: slog
 
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "time"
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
 
 type Config struct {
 	Env         string `yaml:"env" env-default:"local"`
@@ -12,4 +16,23 @@ type HTTPServer struct {
 	Address     string        `yaml:"address" env-default:"localhost:8082"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		panic("config path is required")
+	}
+
+	// Check if the file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file not found")
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+
+	return &cfg
 }
