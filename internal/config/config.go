70d76f0e69ef65494ba344dbdda4d6eded96f420
assilbekov <askhat.assilbekov@gmail.com>
2024-04-17 12:59:27 -0300

    add config structs

---
+++ internal/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+import "time"
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8082"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
