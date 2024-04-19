7e171be4f77dae9da2378497fad9b04de5f74121
assilbekov <askhat.assilbekov@gmail.com>
2024-04-19 10:00:45 -0300

    setup pretty slog

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"url-shortener-rest-practice/internal/config"
 	"url-shortener-rest-practice/internal/http-server/middleware/logger"
+	"url-shortener-rest-practice/internal/lib/logger/handlers/slogpretty"
 	"url-shortener-rest-practice/internal/lib/logger/sl"
 	"url-shortener-rest-practice/internal/storage/sqlite"
 )
@@ -56,7 +57,7 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envProd:
@@ -64,4 +65,16 @@ func setupLogger(env string) *slog.Logger {
 	}
 
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
