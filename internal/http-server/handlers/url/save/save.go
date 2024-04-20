e3294dc1f8d9e87dcce8a05761c39a3ed731d61b
assilbekov <askhat.assilbekov@gmail.com>
2024-04-20 09:37:56 -0300

    finish save new

--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -1,6 +1,7 @@
 package save
 
 import (
+	"errors"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 	"github.com/go-playground/validator/v10"
@@ -9,6 +10,7 @@ import (
 	"url-shortener-rest-practice/internal/lib/api/response"
 	"url-shortener-rest-practice/internal/lib/logger/sl"
 	"url-shortener-rest-practice/internal/lib/random"
+	"url-shortener-rest-practice/internal/storage"
 )
 
 type Request struct {
@@ -65,5 +67,23 @@ func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 		if alias == "" {
 			alias = random.NewRandomString(aliasLength)
 		}
+
+		id, err := urlSaver.SaveURL(req.URL, alias)
+		if errors.Is(err, storage.ErrURLExists) {
+			log.Info("url already exists", slog.String("alias", alias))
+
+			render.JSON(w, r, response.Error("url already exists"))
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to save url", sl.Err(err))
+
+			render.JSON(w, r, response.Error("failed to save url"))
+
+			return
+		}
+
+		log.Info("url saved", slog.Int64("id", id))
 	}
 }
